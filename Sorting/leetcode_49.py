08a59ffc8ad011cd0fcec836d40eece7eb9bd3ef
Jacky0420 <1710915537@qq.com>
2020-12-14 21:42:26 +0800

    Daily LeetCode.

---
+++ Sorting/leetcode_49.py
@@ -0,0 +1,33 @@
+#                                   49. 字母异位词分组
+#
+# 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+#
+# 示例:#
+# 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+# 输出:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+#
+# 说明：
+# 所有输入均为小写字母。
+# 不考虑答案输出的顺序。
+
+
+# Medium
+
+import collections
+
+
+class Solution:
+    # 对每个 str 排序即可
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st)
+        
+        return list(mp.values())
